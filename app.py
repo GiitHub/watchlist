a1e12488bc533fd89d1def6717124d14aa484124
Simpl <2369013090@qq.com>
2022-12-07 14:28:22 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -2,31 +2,12 @@ import os
 import sys
 import click
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
-
-
-
-@app.route('/')
-def index():
-    movies = Movie.query.all()
-    return render_template('index.html', movies=movies)
-
-@app.errorhandler(404) # 传入错误代码
-def page_not_found(e): # 接受异常对象作为参数
-    return render_template('404.html'), 404
-
-# 对于多个模板内都需要使用的变量，我们可以使用 app.context_processor 装饰器注册一个模板上下文处理函数
-@app.context_processor
-def inject_user():
-    user = User.query.first()
-    return dict(user=user)
-
-
 
 
 WIN = sys.platform.startswith('win')
@@ -86,3 +67,64 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo('Done.')
+
+app.config['SECRET_KEY'] = 'dev'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(title) > 60 or len(year) != 4:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+ 
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(title) > 60 or len(year) != 4:
+            flash('Invalid input.')
+            return redirect(url_for('edit'), movie_id=movie_id)
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
+
+
+@app.errorhandler(404) # 传入错误代码
+def page_not_found(e): # 接受异常对象作为参数
+    return render_template('404.html'), 404
+
+# 对于多个模板内都需要使用的变量，我们可以使用 app.context_processor 装饰器注册一个模板上下文处理函数
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
